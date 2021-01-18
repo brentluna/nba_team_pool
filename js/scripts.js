57ef684a413b78a690922863a4ebc6db731cf1a4
Brent Luna <brent.luna@instacart.com>
2021-01-18 15:43:52 -0800

    try new url

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 // const teamsURL = `http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams`;
 
 const teamsURL = 'https://boxscores.vercel.app/api/teams';
+const teamURLNew =
+  'http://data.nba.net/10s/prod/v1/current/standings_conference.json';
+
 const chumps = [
   {
     name: 'Brent',
@@ -150,5 +153,8 @@ const buildTables = async () => {
 };
 
 window.addEventListener('load', (event) => {
+  fetch(teamURLNew)
+    .then((r) => r.json())
+    .then((d) => console.log({ d }));
   buildTables();
 });
