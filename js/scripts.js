a4ff1601f820f1a7b781fecc5d2dc7111e00af41
Brent Luna <brent.luna@instacart.com>
2021-01-18 15:47:23 -0800

    move to https

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 const teamsURL = 'https://boxscores.vercel.app/api/teams';
 const teamURLNew =
-  'http://data.nba.net/10s/prod/v1/current/standings_conference.json';
+  'https://data.nba.net/10s/prod/v1/current/standings_conference.json';
 
 const chumps = [
   {
@@ -36,6 +36,11 @@ const getTeams = async () => {
   const data = await res.json();
   console.log({ data });
   return data;
+};
+
+const getParsedTeamsNew = async () => {
+  const res = await getTeams();
+  const teams = [...res.league.conference.east, ...res.league.conference.west];
 };
 
 const getParsedTeams = async () => {
